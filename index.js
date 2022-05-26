cccd0399a40331f8a5333180aec80607b14fa025
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2022-05-26 20:33:42 +0700

    commit: merubah struktur object firstUser & secondUser

--- index.js
+++ index.js
@@ -2,89 +2,100 @@
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 
-const Monica = {
+const monicaFavoriteColor = new Set(["Yellow", "Pink", "White", "Purple"]);
+const wendyFavoriteColor = new Set(["Blue", "Black", "Gray"]);
+
+const monicaEducation = [
+  {
+    name: "SD 01",
+    city: "Jakarta",
+    graduate: 2016,
+  },
+  {
+    name: "SMP 02",
+    city: "Jakarta",
+    graduate: 2019,
+  },
+  {
+    name: "SMA 03",
+    city: "Tangerang",
+    graduate: null,
+  },
+];
+const wendyEducation = [
+  {
+    name: "SD 02",
+    city: "Jakarta",
+    graduate: 2010,
+  },
+  {
+    name: "SMP 03",
+    city: "Bogor",
+    graduate: 2013,
+  },
+  {
+    name: "SMA 01",
+    city: "Surabaya",
+    graduate: 2016,
+  },
+  {
+    name: "Universitas Maju",
+    city: "Tangerang",
+    graduate: null,
+  },
+];
+
+const monicaFavoriteRestaurant = new Set([
+  "Bento",
+  "Sushi",
+  "Pancake",
+  "Eggy",
+  "Tempura",
+  "Bento",
+  "Eggy",
+  "Padang",
+  "Tteok",
+  "Sushi",
+  "Sushi",
+]);
+const wendyFavoriteRestaurant = new Set([
+  "Tempura",
+  "Bento",
+  "Sushi",
+  "Pancake",
+  "Padang",
+  "Katsu",
+  "Geprek",
+  "Pancake",
+  "Eggy",
+]);
+
+const firstUser = {
   name: "Monica",
   gender: "Female",
   age: 17,
   email: "monica@dingdong.com",
-  favoriteColor: new Set(["Yellow", "Pink", "White", "Purple"]),
-  isHavaPet: true,
-  education: [
-    {
-      name: "SD 01",
-      city: "Jakarta",
-      graduate: 2016,
-    },
-    {
-      name: "SMP 02",
-      city: "Jakarta",
-      graduate: 2019,
-    },
-    {
-      name: "SMA 03",
-      city: "Tangerang",
-      graduate: null,
-    },
-  ],
-  favoriteRestaurant: new Set([
-    "Bento",
-    "Sushi",
-    "Pancake",
-    "Eggy",
-    "Tempura",
-    "Bento",
-    "Eggy",
-    "Padang",
-    "Tteok",
-    "Sushi",
-    "Sushi",
-  ]),
+  favoriteColor: [...monicaFavoriteColor],
+  isHavaPet: "Yes",
+  education: [...monicaEducation],
+  favoriteRestaurant: [...monicaFavoriteRestaurant],
 };
 
-const Wendy = {
+const secondUser = {
   name: "Wendy",
   gender: "Male",
   age: 23,
   email: "wendy@dingdong.com",
-  favoriteColor: new Set(["Blue", "Black", "Gray"]),
-  isHavaPet: false,
-  education: [
-    {
-      name: "SD 02",
-      city: "Jakarta",
-      graduate: 2010,
-    },
-    {
-      name: "SMP 03",
-      city: "Bogor",
-      graduate: 2013,
-    },
-    {
-      name: "SMA 01",
-      city: "Surabaya",
-      graduate: 2016,
-    },
-    {
-      name: "Universitas Maju",
-      city: "Tangerang",
-      graduate: null,
-    },
-  ],
-  favoriteRestaurant: new Set([
-    "Tempura",
-    "Bento",
-    "Sushi",
-    "Pancake",
-    "Padang",
-    "Katsu",
-    "Geprek",
-    "Pancake",
-    "Eggy",
-  ]),
+  favoriteColor: [...wendyFavoriteColor],
+  isHavaPet: "No",
+  education: [...wendyEducation],
+  favoriteRestaurant: [...wendyFavoriteRestaurant],
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [Monica, Wendy];
+const users = [];
+users.push(firstUser);
+users.push(secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
